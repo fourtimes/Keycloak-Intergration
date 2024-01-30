cec294eccf6e9b963331220f0f553e2054e1ef13
fourtimes <91359308+fourtimes@users.noreply.github.com>
2024-01-30 12:49:16 +0530

    Update test-controller.js

--- node-with-confidentials-token/controller/test-controller.js
+++ node-with-confidentials-token/controller/test-controller.js
@@ -6,11 +6,12 @@ router.get('/anonymous', function (req, res) {
     res.send("Hello Anonymous");
 });
 
-router.post('/us', (req, res) => {
-    const {grant_type, client_id, client_secret, username, password} = req.body;
-    console.log(client_id);
+// router.post('/us', (req, res) => {
+//     const {grant_type, client_id, client_secret, username, password} = req.body;
+//     console.log(client_id);
 
-});
+// });
+
 router.get('/user', keycloak.protect('user'), (req, res) => {
     console.log(res)
     res.send("This is the Keycloak employee1 user with User Role ");
